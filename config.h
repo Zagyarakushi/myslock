e1184982193544774aa882885cca6479763b121f
JM <jp123ukalienz@gmail.com>
2020-07-07 18:48:53 +0100

    initial

---
+++ config.h
@@ -0,0 +1,18 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#282828",   /* after initialization */
+	[INPUT] =  "#458588",   /* during input */
+	[FAILED] = "#CC241d",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
